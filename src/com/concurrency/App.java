3f1b9c47a0274cac940d27cf1fe24e461047323a
mohamed-aly <mali902016@gmail.com>
2022-03-22 14:21:20 +0200

    Starting threads I - Runnable

--- src/com/concurrency/App.java
+++ src/com/concurrency/App.java
@@ -1,14 +1,19 @@
 package com.concurrency;
-class Runner1{
-    public void startRunning(){
+class Runner1 implements Runnable{
+
+    @Override
+    public void run() {
         for(int i = 0; i < 10; ++i){
             System.out.println("Runner1 " + i);
         }
     }
 }
 
-class Runner2{
-    public void startRunning(){
+class Runner2 implements Runnable{
+
+
+    @Override
+    public void run() {
         for(int i = 0; i < 10; ++i){
             System.out.println("Runner2 " + i);
         }
@@ -17,10 +22,10 @@ class Runner2{
 
 public class App {
     public static void main(String[] args) {
-        Runner1 runner1 = new Runner1();
-        Runner2 runner2 = new Runner2();
+        Thread runner1 = new Thread(new Runner1());
+        Thread runner2 = new Thread(new Runner2());
 
-        runner1.startRunning();
-        runner2.startRunning();
+        runner1.start();
+        runner2.start();
     }
 }
