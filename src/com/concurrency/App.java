3b971e968082441c5d4b8b65281f1ad43f915217
mohamed-aly <mali902016@gmail.com>
2022-03-22 17:20:13 +0200

    Starting thread by extending Thread class

--- src/com/concurrency/App.java
+++ src/com/concurrency/App.java
@@ -1,29 +1,39 @@
 package com.concurrency;
-class Runner1 implements Runnable{
+class Runner1 extends Thread {
 
     @Override
     public void run() {
-        for(int i = 0; i < 10; ++i){
+        for(int i = 0; i < 100; ++i){
             System.out.println("Runner1 " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 
-class Runner2 implements Runnable{
+class Runner2 extends Thread {
 
 
     @Override
     public void run() {
-        for(int i = 0; i < 10; ++i){
+        for(int i = 0; i < 100; ++i){
             System.out.println("Runner2 " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        Thread runner1 = new Thread(new Runner1());
-        Thread runner2 = new Thread(new Runner2());
+        Thread runner1 = new Runner1();
+        Thread runner2 = new Runner2();
 
         runner1.start();
         runner2.start();
